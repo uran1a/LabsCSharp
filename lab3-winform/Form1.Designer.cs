be26178f6333be8a8bcb99db0c753bcd2c7125b3
uran1a <65703033+uran1a@users.noreply.github.com>
2022-10-05 15:56:10 +0300

    lab3 v2

--- lab3-winform/Form1.Designer.cs
+++ lab3-winform/Form1.Designer.cs
@@ -28,16 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult6 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCalculation6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonCalculation6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult8 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonResult8 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -64,6 +81,11 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.labelResult6);
+            this.tabPage2.Controls.Add(this.numericUpDown2);
+            this.tabPage2.Controls.Add(this.numericUpDown1);
+            this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.buttonCalculation6);
             this.tabPage2.Controls.Add(this.pictureBox1);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
@@ -73,6 +95,68 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Задание 6";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(8, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(292, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сумма полученных значений: ";
+            // 
+            // labelResult6
+            // 
+            this.labelResult6.AutoSize = true;
+            this.labelResult6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResult6.Location = new System.Drawing.Point(300, 147);
+            this.labelResult6.Name = "labelResult6";
+            this.labelResult6.Size = new System.Drawing.Size(0, 28);
+            this.labelResult6.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(166, 104);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(10, 104);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(8, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите отрезок (1-ое < 2-ого):";
+            // 
+            // buttonCalculation6
+            // 
+            this.buttonCalculation6.Location = new System.Drawing.Point(10, 188);
+            this.buttonCalculation6.Name = "buttonCalculation6";
+            this.buttonCalculation6.Size = new System.Drawing.Size(152, 44);
+            this.buttonCalculation6.TabIndex = 1;
+            this.buttonCalculation6.Text = "Найти сумму";
+            this.buttonCalculation6.UseVisualStyleBackColor = true;
+            this.buttonCalculation6.Click += new System.EventHandler(this.buttonCalculation6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab3_winform.Properties.Resources.Screenshot_3;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 57);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // tabPage3
             // 
@@ -85,6 +169,12 @@
             // 
             // tabPage4
             // 
+            this.tabPage4.Controls.Add(this.label3);
+            this.tabPage4.Controls.Add(this.labelResult8);
+            this.tabPage4.Controls.Add(this.numericUpDown4);
+            this.tabPage4.Controls.Add(this.label5);
+            this.tabPage4.Controls.Add(this.buttonResult8);
+            this.tabPage4.Controls.Add(this.pictureBox2);
             this.tabPage4.Location = new System.Drawing.Point(4, 29);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Size = new System.Drawing.Size(795, 418);
@@ -92,24 +182,71 @@
             this.tabPage4.Text = "Задание 8";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // label3
             // 
-            this.pictureBox1.Image = global::lab3_winform.Properties.Resources.Screenshot_3;
-            this.pictureBox1.Location = new System.Drawing.Point(8, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(540, 57);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(8, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 28);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Сумма: ";
             // 
-            // buttonCalculation6
+            // labelResult8
             // 
-            this.buttonCalculation6.Location = new System.Drawing.Point(8, 82);
-            this.buttonCalculation6.Name = "buttonCalculation6";
-            this.buttonCalculation6.Size = new System.Drawing.Size(94, 29);
-            this.buttonCalculation6.TabIndex = 1;
-            this.buttonCalculation6.Text = "button1";
-            this.buttonCalculation6.UseVisualStyleBackColor = true;
-            this.buttonCalculation6.Click += new System.EventHandler(this.buttonCalculation6_Click);
+            this.labelResult8.AutoSize = true;
+            this.labelResult8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResult8.Location = new System.Drawing.Point(95, 117);
+            this.labelResult8.Name = "labelResult8";
+            this.labelResult8.Size = new System.Drawing.Size(64, 28);
+            this.labelResult8.TabIndex = 9;
+            this.labelResult8.Text = "adasd";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(123, 81);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown4.TabIndex = 10;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(8, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Введите N:";
+            // 
+            // buttonResult8
+            // 
+            this.buttonResult8.Location = new System.Drawing.Point(8, 157);
+            this.buttonResult8.Name = "buttonResult8";
+            this.buttonResult8.Size = new System.Drawing.Size(152, 44);
+            this.buttonResult8.TabIndex = 7;
+            this.buttonResult8.Text = "Найти сумму";
+            this.buttonResult8.UseVisualStyleBackColor = true;
+            this.buttonResult8.Click += new System.EventHandler(this.buttonResult8_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(687, 67);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
@@ -121,7 +258,14 @@
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +279,16 @@
         private TabPage tabPage3;
         private TabPage tabPage4;
         private Button buttonCalculation6;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Label labelResult6;
+        private Label label2;
+        private Label label3;
+        private Label labelResult8;
+        private NumericUpDown numericUpDown4;
+        private Label label5;
+        private Button buttonResult8;
+        private PictureBox pictureBox2;
     }
 }
--- lab3-winform/Form1.cs
+++ lab3-winform/Form1.cs
@@ -5,11 +5,41 @@ namespace lab3_winform
         public Form1()
         {
             InitializeComponent();
+            labelResult6.Visible = false;
+            labelResult8.Visible = false;
         }
 
         private void buttonCalculation6_Click(object sender, EventArgs e)
         {
+            static double func(int x)
+            {
+                return (2 * Math.Pow(x, 2)) + (108 / Math.Pow(x, 2)) - 59;
+            }
+            try
+            {
+                int startSegment = Convert.ToInt32(numericUpDown1.Value);
+                int endSegment = Convert.ToInt32(numericUpDown2.Value);
+                if (startSegment >= endSegment) throw new Exception("Не верный отрезок!");
+                double sum = 0;
+                for (int i = startSegment; i <= endSegment; i++)
+                    sum += func(i);
+                labelResult6.Text = Convert.ToString(Math.Round(sum,2));
+                labelResult6.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: "+ex.Message);
+            }
+        }
 
+        private void buttonResult8_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(numericUpDown4.Value);
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+                sum += Convert.ToInt32(Math.Pow(i, n - i + 1));
+            labelResult8.Text = Convert.ToString(sum);
+            labelResult8.Visible = true;
         }
     }
 }
