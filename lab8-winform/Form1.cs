a1322d3cb3a5c2a7286a288181ad0cb9dc5aa6be
uran1a <65703033+uran1a@users.noreply.github.com>
2022-11-16 22:54:18 +0300

    dev lab8

--- lab8-winform/Form1.cs
+++ lab8-winform/Form1.cs
@@ -5,6 +5,22 @@ namespace lab8_winform
         public Form1()
         {
             InitializeComponent();
+            //Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = @"C:\Users\voron\source\repos\LabsCSharp\lab8-winform";
+
+            Console.WriteLine(docPath);
+            var dirName = $@"{path}\test";
+
+            DirectoryInfo di = Directory.CreateDirectory(dirName);
+            Console.WriteLine($"Full name: {di.FullName}, Name: {di.Name}, Parent: {di.Parent}");
+
+            if (Directory.Exists(dirName))
+                Console.WriteLine("Directory exists");
+            else
+                Console.WriteLine("Directory does not exist");
+            //string path = @"C:\Users\voron\source\repos\LabsCSharp\lab8-winform";
+            //System.IO.Directory.CreateDirectory(path);
         }
     }
 }
